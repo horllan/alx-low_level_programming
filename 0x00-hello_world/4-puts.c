32f94b66eb463d884712d54c0f0e64deb025801f
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-10-13 19:30:39 -0700

    finished

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@ int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 /**
- *return - Always return 0
+* Return - statement "Always return zero"
  */
 return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *  * print_hello - Prints "use printf to out given text"
+  */
+void main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+/**
+ *     * Return: 1 if the number is positive. 0 otherwise
+ */
+		return (nb > 0);
+}
