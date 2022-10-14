b747799823fb3b731e6d465fb1b629e8449253f5
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-10-13 21:28:01 -0700

    done

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,18 @@
+/*task prints the size of the data type based on th
+ *  * main - program execution starts here
+ *   *task prints the size of the data type based on th
+ *   e machine
+ *    * Return: Return zero for program success
+ *      *
+ *      *{
+ *      	*/
 
-#include <stdio.h>
-/**
- * main - program execution starts here
- *task prints the size of the data type based on the machine
- * Return: Return zero for program success
-  */
 int main(void)
 {
-	printf("Size of a char: %d  byte(s)\n", sizeof(char));
-		printf("Size of an int: %d byte(s)\n", sizeof(int));
-			printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
-				printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-				printf("Size of a float: %d byte(s)\n", sizeof(float));
-						return (0);
+		printf("Size of a char: %d byte(s)\n", sizeof(char));
+			printf("Size of an int: %d byte(s)\n", sizeof(int));
+				printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
+					printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+						printf("Size of a float: %d byte(s)\n", sizeof(float));
+							return (0);
 }
