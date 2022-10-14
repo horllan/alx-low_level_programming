f91af7ebd2de2b7382b9435e65fe3372e4cc4363
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-10-13 19:14:03 -0700

    added description to function and return

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ *main - puts "Programming is like building a multilingual puzzle"
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+/**
+ *return - Always return 0
+ */
+return (0);
 }
