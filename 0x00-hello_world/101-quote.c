6c3a7115ac423636237acf3b9591806360f695fe
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-10-16 10:18:35 -0700

    modified task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
- *  * main -Program starting point
- *   *
- *    * Return: program returns  1 
+ * main - Program starting point
+   * Return: This program returns 1 
  *     */
 int main(void)
 {
-	char str1 = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str1, 59);
+	write(2, quote, 59);
 	return (1);
 }
