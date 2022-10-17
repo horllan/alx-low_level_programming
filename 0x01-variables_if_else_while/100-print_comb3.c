cd85db091acc12e38cc2da0a647d7a9b6477e2a1
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-10-17 16:21:43 -0700

    modified task 100 code to remove the trailing comma

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,23 +11,23 @@ int main(void)
 {
 int i, j;
 
-for (i = '0'; i < '9'; i++)
+for (i = '0'; i <= '9'; i++)
 {
 for (j = '0'; j <= '9'; j++)
 {
+if (i < j)
+{
 putchar(i);
 putchar(j);
-if (i != '9')
+if (i != '8')
 {
 putchar(',');
 putchar(' ');
 }
-if (i > j)
-{
-	continue;
 }
 }
 }
+
 putchar('\n');
 return (0);
 }
