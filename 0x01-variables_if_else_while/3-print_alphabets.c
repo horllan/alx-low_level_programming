f43c810b83e1ccd332b5f88b196dbff7b1808de8
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-10-17 12:34:51 -0700

    task accomplished

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,5 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
 /**
@@ -10,20 +8,18 @@
  * Return: always 0 for program success
  */
 int main(void)
-
 {
+int small, cap;
 
-	int small, cap;
-	
-	for (small = 97 ; small <= 122 ; small++)
-	{
-		putchar(small);
-	}
-	for (cap = 65; cap < 91; cap++)
-	{
-		putchar(cap);
-	}
-	putchar('\n');
-	return (0);
+for (small = 97 ; small <= 122 ; small++)
+{
+putchar(small);
+}
+for (cap = 65; cap < 91; cap++)
+{
+putchar(cap);
+}
+putchar('\n');
+return (0);
 
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,30 +1,23 @@
 #include <stdlib.h>
 
-#include <time.h>
-
-#include <stdio.h>
-
 /**
  * main - program start execution here
  * prints small alphabets
  * Return: always 0 for program success
  */
-int main (void)
-
+int main(void)
 {
+int start;
 
-	int start; 
-
-	for (start = 97; start <= 122; start++)
-	{
-		if ( start == 101 || start == 113)
-		{
-			continue;
-		}
-		putchar(start);
-	}
-	putchar('\n');
-
-	return (0);
+for (start = 97; start <= 122; start++)
+{
+if (start == 101 || start == 113)
+{
+continue;
+}
+putchar(start);
+}
+putchar('\n');
+return (0);
 
 }
