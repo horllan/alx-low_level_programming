3dd3b13e1e7e5c7aa2513e7ae32336cfcb298801
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-10-18 13:05:37 -0700

    task accomplished

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *  output - Prints _putchar
+ * Description - prints putchar
+ *   Return: void
+ */
+int output(void)
+{
+printf("_putchar\n");
+return (0);
+}
+
+/**
+ * main - entry point
+ * Description: Starting point
+ * Return: returns  always  returns 0 for success
+ */
+
+int main(void)
+{
+output();
+
+return (0);
+}
