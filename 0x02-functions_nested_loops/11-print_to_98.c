d2f276d00d5f02843c205e46f79472cc52e09c16
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-10-19 18:05:20 -0700

    task 11 done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,19 +8,29 @@
 
 void print_to_98(int n)
 {
-while (n < 98)
+if ( n < 98)
 {
-_putchar(n + '0');
-
+while (n <= 98)
+{
+if (n == 98)
+{
+printf("%d\n, ", n);
+}
+printf("%d, ", n);
 n++;
 }
+}
 
-while (n > 98)
+if (n >= 98)
 {
-_putchar(n + '0');
+while (n >= 98)
+{
+if (n ==98)
+{
+printf("%d\n, ", n);
+}
+printf("%d, " n);
 n--;
 }
-_putchar(n + '0');
-_putchar('\n');
 }
-
+}
