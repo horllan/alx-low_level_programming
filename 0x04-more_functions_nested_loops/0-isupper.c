0c336c35c6e4498c98bf1c3fcf4770af3dc43529
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-10-20 17:04:19 -0700

    task 0 and 1 done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _upper - starting point
+ * Description: checks if a character is in uppercase
+ * @c: value to check
+ * Return: 1 if c is lowercase otherwise return 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _upper - starting point
+ * Description: checks if a character is in uppercase
+ * @c: value to check
+ * Return: 1 if c is lowercase otherwise return 0
+ */
+
+int _isdigit(int c)
+{
+if (c >= '48' && c <= '57')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
