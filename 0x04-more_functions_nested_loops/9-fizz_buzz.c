334fcda2ca547d4f587c6c40b6eacf2e858c4551
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-10-21 19:28:59 -0700

    task 9 done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+*main - prints number to 100 but print
+*or fizz or buzz or fizzBuzzim when divisible by 3, 5 and 15
+*Return: returns 0
+*/
+int main(void)
+{
+int num;
+while (num++ < 100)
+
+if ((num % 15 == 0)
+printf("FizzBuzz ");
+
+else if ((num % 3) == 0)
+printf("Fizz ");
+else if ((num % 5) == 0)
+{
+if (num != 100)
+printf("Buzz ");
+
+else
+printf("Buzz");
+}
+
+else
+printf("%d ", num);
+
+printf("\n");
+return (0);
+}
