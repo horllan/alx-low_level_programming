1bc922e3570c605e87009e434130fe491e72228f
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-10-21 17:50:12 -0700

    modified task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,10 +4,10 @@
 
 void positive_or_negative(int i);
 
-int largest_number(int a, int b, int c);
+int largest_number(int, int, int);
 
-int convert_day(int month, int day);
+int convert_day(int, int);
 
-void print_remaining_days(int month, int day, int year);
+void print_remaining_days(int, int, int);
 
 #endif
