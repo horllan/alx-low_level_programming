45d7dac5b482a7524b8fc460c1d971fe30ce3ee7
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-11-02 12:45:46 -0700

    done

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print reverse function using recursion in reverse
+ * @s: input char
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+f (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(*s);
+}
+}
