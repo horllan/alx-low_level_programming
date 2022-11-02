062d5adb55781bdfd103f5b7342dd541b2099448
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-11-02 12:37:52 -0700

    task 0 done

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - puts char function using recursion
+ * @s: input char
+ */
+
+
+void _puts_recursion(char *s)
+{
+
+if (s[0] == '\0')
+_putchar(10);
+else
+_putchar(s[0]);
+_puts_recursion(s + 1);
+
+}
