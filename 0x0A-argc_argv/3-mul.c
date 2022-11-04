beb75829f8b255ab418442b2116a1d28c71bff3a
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-11-04 11:47:41 -0700

    done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,21 +2,26 @@
 #include <stdio.h>
 
 /**
- * main - this program just print the multiplication 
- * of the two arhuments passed in
- * @argc: argument count
- * @argv: argument variable array whichvis same size as argc
- * Return: 0 for success
- */
+* main - this program just print the multiplication 
+* of the two arhuments passed in
+* @argc: argument count
+* @argv: argument variable array whichvis same size as argc
+* Return: 0 for success
+*/
 
 int main(int argc, const char *argv[])
 {
-if (argc >2)
+if (argc > 1)
 {
 int f = atoi(argv[1]);
 int l = atoi(argv[2]);
 int res = f * l;
 printf("%d\n", res);
-}
 return (0);
+}
+else
+{
+printf("Error")
+return (1);
+}
 }
