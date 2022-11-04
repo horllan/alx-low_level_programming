cb09a884615f7d3cb14cbbd434a03fcd74348abd
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-11-04 10:25:14 -0700

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - this program just print the number of;arguments passed in
+ * @argc: argument count
+ * @argv: argument variable array whichvis same size as argc
+ * Return: 0 for success
+ */
+
+int main(int argc, char* argv[])
+{
+printf("%d\n", argc]);
+return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - this program just print all the arguments passed in
+ * @argc: argument count
+ * @argv: argument variable array whichvis same size as argc
+ * Return: 0 for success
+ */
+
+int main(int argc, char* argv[])
+{
+int i = 0;
+
+while (i < argc)
+{
+printf("%s\n", argv[i]);
+i++;
+}
+return (0);
+}
