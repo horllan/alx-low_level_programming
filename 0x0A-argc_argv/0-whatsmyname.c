104e5df36b958a4cd52633a8a763eecd88457483
makanjuola funmibi <makanjuolafunmibi@gmail.com>
2022-11-04 10:00:36 -0700

    task 0 done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - this program just print the name of the file using the argv
+ * @argc: argument count
+ * @argv: argument variable array whichvis same size as argc
+ * Return: 0 for success
+ */
+
+int main(int argc, char* argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Program on argument variables
